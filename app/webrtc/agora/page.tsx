cbddd6af3921eb6ac1b74b5945d73957c610336b
jhlee <jhlee@spicusinc.com>
2024-06-20 19:01:40 +0900

    기능 구현 :: Agora 앱빌더 화면 구현

--- app/webrtc/agora/page.tsx
+++ app/webrtc/agora/page.tsx
@@ -1,51 +1,47 @@
 'use client';
 
 import Header from "@/components/Header";
-import { FormEvent } from "react";
+import clsx from "clsx";
+import { FormEvent, useState } from "react";
 
 export default function Page() {
+  const [isHost, setIsHost] = useState(true);
+
   function handleSubmit(event: FormEvent<HTMLFormElement>): void {
     throw new Error("Function not implemented.");
+
+    event.preventDefault();
+  }
+
+  function handleClick(isTrue: boolean) {
+    setIsHost(isTrue);
   }
 
   return (
 		<div className="flex flex-col justify-center items-center h-full gap-2">
       <Header />
       <div className="flex flex-col justify-center items-center gap-4 mt-12 w-full max-w-xl h-full">
-        <div className="flex flex-col gap-4 p-4">
-          <h1 className="text-xl font-bold">WebRTC example: Agora</h1>
-        </div>
-        <div className="flex flex-col gap-4 p-4">
-        <div className="flex flex-col items-center">
-          <form onSubmit={handleSubmit}>
-            <div className="flex items-center gap-4">
-              <div>
-                <label
-                  className="text-gray-700 font-bold"
-                  htmlFor="inline-full-name"
-                >
-                  Channel Name
-                </label>
-              </div>
-              <div>
-                <input
-                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
-                  id="inline-full-name"
-                  type="text"
-                  name="channel"
-                  placeholder="Enter channel name"
-                  required
-                />
-              </div>
-            </div>
-            <div className="text-center">
-              <button
-                className="inline-flex items-center justify-center px-5 py-3 mt-5 text-base font-medium text-center text-white bg-gray-500 rounded-lg hover:bg-gray-700 focus:ring-4 focus:ring-gray-700 dark:focus:ring-gray-700"
-              >Submit</button>
-            </div>
-          </form>
-        </div>
-        </div>
+        <form onSubmit={handleSubmit} className="flex flex-col items-center gap-4 w-full">
+          <h1 className="text-xl font-bold">Video Call Test</h1>
+          <input type="text" id="username" className="border border-gray-700 rounded px-4 py-2 w-full" placeholder="Enter a username" required />
+          <input type="text" id="channel-title" className="border border-gray-700 rounded px-4 py-2 w-full" placeholder="Enter a title" required />
+          <div className="flex justify-center items-center gap-2 w-full">
+            <button
+              type="button"
+              onClick={() => handleClick(true)}
+              className={clsx("border border-black rounded px-4 py-2 w-full hover:bg-gray-500", { "bg-gray-500 text-white" : isHost })}
+            >Host</button>
+            <button
+              type="button"
+              onClick={() => handleClick(false)}
+              className={clsx("border border-black rounded px-4 py-2 w-full hover:bg-gray-500", { "bg-gray-500 text-white" : !isHost })}
+            >Viewer</button>
+          </div>
+          <button
+            type="submit"
+            className="border border-black rounded px-4 py-2 w-full hover:bg-gray-500"
+          >Join</button>
+        </form>
       </div>
     </div>
   );
